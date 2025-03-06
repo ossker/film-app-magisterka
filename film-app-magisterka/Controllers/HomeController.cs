de0023d5913a22a9aa949d8598cd9735bfa7f0fb
ossker <oskar.bienko@onet.pl>
2025-03-06 09:00:26 +0100

    Add project files.

---
+++ film-app-magisterka/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using film_app_magisterka.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace film_app_magisterka.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ film-app-magisterka/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace film_app_magisterka.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
